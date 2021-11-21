64cf678eae0fac1d03cc6c6db596338a18034ca9
2018432 <2018432@brunel.ac.uk>
2021-11-21 22:11:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Excercise 1
+hey = "hey its jo!"
+there = "there"
+
+print(hey[:4])
+print(hey[-1])
+print(hey*2)
+print(hey[:-1] + "there" + hey[-1])
+print(hey[:-1] + ' ' + 'there' + hey[-1])
+print(there [1])
+print(hey[4])
+print(hey *2 + 'there' [:-1] + 'there' + hey [-1])
+print(hey[-2:1])
+
+
+
